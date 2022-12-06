9cefcaf633691dbbd5fd1776b57bc0b6cba25cff
Avinash-DM <Avinashdmacc@gmail.com>
2022-12-06 07:30:10 +0530

    db connection

--- 06springbtpdb/src/main/java/com/btptrianings/demo/Application.java
+++ 06springbtpdb/src/main/java/com/btptrianings/demo/Application.java
@@ -2,7 +2,7 @@ package com.btptrianings.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//git test
 @SpringBootApplication
 public class Application {
 
